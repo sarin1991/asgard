7da26740a71b1618ea586fca212bff53cd649c54
Sarin Eapen Chandy <sarin1991@gmail.com>
2022-06-23 09:56:39 -0500

    Need to add term to timers

--- src/messages.rs
+++ src/messages.rs
@@ -3,13 +3,13 @@ use crate::protobuf_messages::asgard_messages::{LeaderSync,FollowerInitialized,L
 
 
 #[derive(Clone,Debug)]
-pub struct AsgardMessageTimer{
-
+pub(crate) struct AsgardMessageTimer{
+    pub(crate) term:u64,
 }
 
 #[derive(Clone,Debug)]
-pub struct AsgardElectionTimer{
-
+pub(crate) struct AsgardElectionTimer{
+    pub(crate) term:u64,
 }
 
 #[derive(Clone,Debug)]
@@ -25,6 +25,24 @@ pub enum AsgardianMessage{
     AddEntry(AddEntry),
     AsgardMessageTimer(AsgardMessageTimer),
     AsgardElectionTimer(AsgardElectionTimer),
+}
+
+impl AsgardianMessage {
+    pub(crate) fn get_message_term(&self) -> u64 {
+        match self {
+            AsgardianMessage::LeaderSync(leader_sync) => leader_sync.term,
+            AsgardianMessage::FollowerInitialized(follower_initialized) => follower_initialized.term,
+            AsgardianMessage::LeaderHeartbeat(leader_heartbeat) => leader_heartbeat.term,
+            AsgardianMessage::VoteResponse(vote_response) => vote_response.term,
+            AsgardianMessage::VoteRequest(vote_request) => vote_request.term,
+            AsgardianMessage::RebellionResponse(rebellion_response) => rebellion_response.term,
+            AsgardianMessage::RebellionRequest(rebellion_request) => rebellion_request.term,
+            AsgardianMessage::FollowerUpdate(follower_update) => follower_update.term,
+            AsgardianMessage::AddEntry(add_entry) => add_entry.term,
+            AsgardianMessage::AsgardMessageTimer(asgard_message_timer) => asgard_message_timer.term,
+            AsgardianMessage::AsgardElectionTimer(asgard_election_timer) => asgard_election_timer.term,
+        }
+    }
 }
 
 #[derive(Clone,Debug)]
--- src/protobuf_messages/asgard_messages.rs
+++ src/protobuf_messages/asgard_messages.rs
@@ -44,87 +44,101 @@ pub mod asgard_log_message {
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct AddEntry {
-    #[prost(message, repeated, tag="1")]
+    #[prost(uint64, tag="1")]
+    pub term: u64,
+    #[prost(message, repeated, tag="2")]
     pub messages: ::prost::alloc::vec::Vec<AsgardLogMessage>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct LeaderHeartbeat {
-    #[prost(string, tag="1")]
+    #[prost(uint64, tag="1")]
+    pub term: u64,
+    #[prost(string, tag="2")]
     pub leader_id: ::prost::alloc::string::String,
-    #[prost(uint64, tag="2")]
+    #[prost(uint64, tag="3")]
     pub commit_index: u64,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct FollowerUpdate {
     #[prost(uint64, tag="1")]
+    pub term: u64,
+    #[prost(uint64, tag="2")]
     pub log_index: u64,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RebellionRequest {
-    #[prost(string, tag="1")]
+    #[prost(uint64, tag="1")]
+    pub term: u64,
+    #[prost(string, tag="2")]
     pub candidate_id: ::prost::alloc::string::String,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct VoteRequest {
-    #[prost(string, tag="1")]
+    #[prost(uint64, tag="1")]
+    pub term: u64,
+    #[prost(string, tag="2")]
     pub candidate_id: ::prost::alloc::string::String,
-    #[prost(uint64, tag="2")]
-    pub last_log_index_term: u64,
     #[prost(uint64, tag="3")]
+    pub last_log_index_term: u64,
+    #[prost(uint64, tag="4")]
     pub last_log_index: u64,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RebellionResponse {
-    #[prost(string, tag="1")]
+    #[prost(uint64, tag="1")]
+    pub term: u64,
+    #[prost(string, tag="2")]
     pub peer_id: ::prost::alloc::string::String,
-    #[prost(bool, tag="2")]
+    #[prost(bool, tag="3")]
     pub vote_granted: bool,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct VoteResponse {
-    #[prost(string, tag="1")]
-    pub candidate_id: ::prost::alloc::string::String,
-    #[prost(uint64, tag="2")]
+    #[prost(uint64, tag="1")]
     pub term: u64,
+    #[prost(string, tag="2")]
+    pub candidate_id: ::prost::alloc::string::String,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct LeaderSync {
-    #[prost(message, repeated, tag="1")]
+    #[prost(uint64, tag="1")]
+    pub term: u64,
+    #[prost(message, repeated, tag="2")]
     pub messages: ::prost::alloc::vec::Vec<AsgardLogMessage>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct FollowerInitialized {
-    #[prost(string, tag="1")]
+    #[prost(uint64, tag="1")]
+    pub term: u64,
+    #[prost(string, tag="2")]
     pub leader_id: ::prost::alloc::string::String,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct GenericAsgardMessage {
-    #[prost(uint64, tag="1")]
-    pub term: u64,
-    #[prost(oneof="generic_asgard_message::GenericMessage", tags="2, 3, 4, 5, 6, 7, 8, 9, 10")]
+    #[prost(oneof="generic_asgard_message::GenericMessage", tags="1, 2, 3, 4, 5, 6, 7, 8, 9")]
     pub generic_message: ::core::option::Option<generic_asgard_message::GenericMessage>,
 }
 /// Nested message and enum types in `GenericAsgardMessage`.
 pub mod generic_asgard_message {
     #[derive(Clone, PartialEq, ::prost::Oneof)]
     pub enum GenericMessage {
-        #[prost(message, tag="2")]
+        #[prost(message, tag="1")]
         AddEntryMessage(super::AddEntry),
-        #[prost(message, tag="3")]
+        #[prost(message, tag="2")]
         HeartBeatMessage(super::LeaderHeartbeat),
-        #[prost(message, tag="4")]
+        #[prost(message, tag="3")]
         FollowerUpdateMessage(super::FollowerUpdate),
-        #[prost(message, tag="5")]
+        #[prost(message, tag="4")]
         CanvassRebellionMessage(super::RebellionRequest),
-        #[prost(message, tag="6")]
+        #[prost(message, tag="5")]
         RequestVotesMessage(super::VoteRequest),
-        #[prost(message, tag="7")]
+        #[prost(message, tag="6")]
         CanvassResponseMessage(super::RebellionResponse),
-        #[prost(message, tag="8")]
+        #[prost(message, tag="7")]
         VoteResponseMessage(super::VoteResponse),
-        #[prost(message, tag="9")]
+        #[prost(message, tag="8")]
         LeaderSyncMessage(super::LeaderSync),
-        #[prost(message, tag="10")]
+        #[prost(message, tag="9")]
         FollowerInitializedMessage(super::FollowerInitialized),
     }
 }
